5f8bac5743bdabe9043c0dad7cf126c657a5e5be
Rafael Fagundes Oliveira <rafael.fagundes@cwi.com.br>
2026-05-16 16:15:55 -0300

    first commit

---
+++ src/main/java/com/ieka/autoanima/AutoanimaApplication.java
@@ -0,0 +1,13 @@
+package com.ieka.autoanima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoanimaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoanimaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ieka/autoanima/AutoanimaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ieka.autoanima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoanimaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
